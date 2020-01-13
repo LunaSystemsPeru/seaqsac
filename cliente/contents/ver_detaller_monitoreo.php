7db66985a3da286011a8345016b1ce4477b3a44b
Bruno <1994.brunoalva@gmail.com>
2020-01-13 18:36:29 -0500

    se agrego la vista de los monitoreos

--- cliente/contents/ver_detaller_monitoreo.php
+++ cliente/contents/ver_detaller_monitoreo.php
@@ -1,10 +1,51 @@
 <?php
 session_start();
 require '../../models/Monitoreo.php';
+require '../../models/MonitoreoComentario.php';
+require '../../models/MonitoreoAnexo.php';
+require '../../models/Cliente.php';
+require '../../models/ClienteSucursal.php';
+require '../../models/TipoClasificacion.php';
+require '../../models/TipoSubClase.php';
+require '../../models/Equipo.php';
+require '../../models/MonitoreoEquipo.php';
 $c_monitoreo = new Monitoreo();
 $c_monitoreo->setIdMonitoreo(filter_input(INPUT_GET, 'monitoreio'));
 
 $c_monitoreo->obtener_datos();
+
+
+$c_comentario = new MonitoreoComentario();
+$c_comentario->setIdMonitoreo($c_monitoreo->getIdMonitoreo());
+
+
+$c_anexo = new MonitoreoAnexo();
+$c_anexo->setIdMonitoreo($c_monitoreo->getIdMonitoreo());
+
+
+$c_cliente = new Cliente();
+$c_cliente->setIdCliente($c_monitoreo->getIdCliente());
+$c_cliente->obtener_datos();
+
+
+$c_sucursal = new ClienteSucursal();
+$c_sucursal->setIdCliente($c_monitoreo->getIdCliente());
+$c_sucursal->setIdSucursal($c_monitoreo->getIdSucursal());
+$c_sucursal->obtener_datos();
+
+$c_clase = new TipoSubClase();
+$c_clase->setIdClase($c_monitoreo->getIdClase());
+$c_clase->obtener_datos();
+
+
+$c_tipo = new TipoClasificacion();
+$c_tipo->setId($c_clase->getIdTipo());
+$c_tipo->obtener_datos();
+
+$c_equipos = new Equipo();
+
+$c_mequipos = new MonitoreoEquipo();
+$c_mequipos->setIdMonitoreo($c_monitoreo->getIdMonitoreo());
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -30,7 +71,9 @@ $c_monitoreo->obtener_datos();
     <!-- endinject -->
     <link rel="shortcut icon" href="../../vendors/assets/images/favicon.png"/>
     <style>
-        .clickclet{
+        .click{
+            color: white;
+            padding: 5px;
             cursor: pointer;
         }
         .clickclet>.card:hover{
@@ -54,7 +97,7 @@ $c_monitoreo->obtener_datos();
                     <div class="col-lg-12 grid-margin stretch-card">
                         <div class="card">
                             <div class="card-header">
-                                <h4 class="h3">Detalle</h4>
+                                <h4 class="h3">Datos del informe</h4>
                                 <!--button class="btn btn-outline-success">
                                     <i class="fa fa-search"></i>Buscar por Criterios
                                 </button-->
@@ -68,18 +111,32 @@ $c_monitoreo->obtener_datos();
                 <div class="row">
 
                     <div class="col-md-4">
-                        <div class="form-group">
-                            <h4 for="inputEmail3" class="col-sm-12 control-label">Fecha Registro: <?php echo $c_monitoreo->getFecha()?></h4>
-                        </div>
-                        <div class="form-group">
-                            <h4 for="inputEmail3" class="col-sm-12 control-label">Fecha Revision: <?php echo $c_monitoreo->getFechaRevision()?></h4>
-                        </div>
-                        <div class="form-group">
-                            <h4 for="inputEmail3" class="col-sm-12 control-label">Estado: <div class="badge badge-success">Activo</div></h4>
+                        <div class="card">
+
+                            <?php
+                            if ($c_monitoreo->getestado() == 1) {
+                                $valor_estado = '<label class="badge badge-warning">Pendiente</label>';
+                            }
+                            $url_informe = "../archivos/clientes/monitoreos/" . $c_monitoreo->getIdCliente() . "/" . $c_monitoreo->getIdSucursal() . "/" . $c_monitoreo->getUrlInforme();
+                            $temporary = explode(".", $c_monitoreo->getUrlInforme());
+                            $file_extension = end($temporary);
+                            $nombre_archivo = $c_cliente->getRazonSocial() . " - " . $c_sucursal->getNombre() . " - " . $c_tipo->getNombre() . " - " . $c_clase->getNombre() . "." . $file_extension;
+                            ?>
+                            <div class="card-body">
+                                <p><span class="font-weight-bold">cliente =</span> <?php echo $c_cliente->getRazonSocial() ?></p>
+                                <p><span class="font-weight-bold">ubicacion =</span> <?php echo $c_sucursal->getNombre() ?></p>
+                                <p><span class="font-weight-bold">direccion =</span> <?php echo $c_sucursal->getDireccion() ?></p>
+                                <p><span class="font-weight-bold">fecha =</span> <?php echo $c_monitoreo->getfecha() ?></p>
+                                <p><span class="font-weight-bold">tipo =</span> <?php echo $c_tipo->getNombre() . " - " . $c_clase->getNombre() ?></p>
+
+                                <p><span class="font-weight-bold">estado =</span> <?php echo $valor_estado ?></p>
+                                <span class="badge badge-primary click" >Ver Informe</span>
+                                <span class="badge badge-info click" >Ver Revisiones</span>
+                            </div>
                         </div>
                     </div>
                     <div class="col-md-8">
-                        <embed src="<?php echo $c_monitoreo->getUrlInforme()?>" type="application/pdf" width="100%" height="600px" />
+                        <embed src="<?php echo '../../archivos/clientes/monitoreos/' . $_SESSION['id_cliente'] . '/' . $c_monitoreo->getIdSucursal() . '/'. $c_monitoreo->getUrlInforme()?>" type="application/pdf" width="100%" height="600px" />
                     </div>
                 </div>
 
